6085c5f566ef91ce78c28accf620322b94603c80
Joshua Lopez <jrenelopez92@gmail.com>
2021-09-16 20:07:08 -0500

    refractored app and searchbar

--- src/components/App.js
+++ src/components/App.js
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
 import SearchBar from "./SearchBar";
 import youtube from "./youtube";
 import VideoList from "./VideoList";
@@ -6,49 +6,72 @@ import VideoDetail from "./VideoDetail";
 
 //TODO for after break, learn grid system with semantic ui
 //TODO fix default video when we re search another video
-class App extends React.Component {
-  state = { videos: [], selectedVideo: null };
 
-  componentDidMount() {
-    this.onTermSubmit("star wars");
-  }
+function App() {
+  const [videos, setVideos] = useState([]);
+  const [selectedVideo, setSelectedVideo] = useState(null);
 
-  onTermSubmit = async (term) => {
+  useEffect(() => {
+    onTermSubmit("star wars");
+  }, []);
+  const onTermSubmit = async (term) => {
     const response = await youtube.get("/search", {
       params: {
         q: term,
       },
     });
-    this.setState({
-      videos: response.data.items,
-      selectedVideo: response.data.items[0],
-    });
+
+    setVideos(response.data.items);
+    setSelectedVideo(response.data.items[0]);
+  };
+  const onVideoSelect = (video) => {
+    setSelectedVideo(video);
   };
 
-  onVideoSelect = (video) => {
-    this.setState({ selectedVideo: video });
-  };
-
-  render() {
-    return (
-      <div className="ui container">
-        <SearchBar onFormSubmit={this.onTermSubmit} />{" "}
-        <div className="ui grid">
-          <div className="ui row">
-            <div className="eleven wide column">
-              <VideoDetail video={this.state.selectedVideo} />
-            </div>
-            <div className="five wide column">
-              <VideoList
-                onVideoSelect={this.onVideoSelect}
-                videos={this.state.videos}
-              />
-            </div>
+  return (
+    <div className="ui container">
+      <SearchBar onFormSubmit={onTermSubmit} />{" "}
+      <div className="ui grid">
+        <div className="ui row">
+          <div className="eleven wide column">
+            <VideoDetail video={selectedVideo} />
+          </div>
+          <div className="five wide column">
+            <VideoList onVideoSelect={onVideoSelect} videos={videos} />
           </div>
         </div>
       </div>
-    );
-  }
+    </div>
+  );
 }
+
+// class App extends React.Component {
+//   state = { videos: [], selectedVideo: null };
+
+//   onVideoSelect = (video) => {
+//     this.setState({ selectedVideo: video });
+//   };
+
+//   render() {
+//     return (
+//       <div className="ui container">
+//         <SearchBar onFormSubmit={this.onTermSubmit} />{" "}
+//         <div className="ui grid">
+//           <div className="ui row">
+//             <div className="eleven wide column">
+//               <VideoDetail video={this.state.selectedVideo} />
+//             </div>
+//             <div className="five wide column">
+//               <VideoList
+//                 onVideoSelect={this.onVideoSelect}
+//                 videos={this.state.videos}
+//               />
+//             </div>
+//           </div>
+//         </div>
+//       </div>
+//     );
+//   }
+// }
 
 export default App;
--- src/components/SearchBar.jsx
+++ src/components/SearchBar.jsx
@@ -1,41 +1,62 @@
 import React, { useState } from "react";
 
-const SearchBar = () => {
+const SearchBar = ({ onFormSubmit }) => {
   const [term, setTerm] = useState("");
+
+  const onInputChange = (event) => {
+    setTerm(event.target.value);
+  };
+  const onSubmit = (event) => {
+    event.preventDefault();
+    onFormSubmit(term);
+  };
+
+  return (
+    <div className="search-bar ui segment">
+      <form onSubmit={onSubmit} action="" className="ui form">
+        <div className="field">
+          <label>videoSearch</label>
+          <input
+            type="text"
+            value={term}
+            onChange={(event) => setTerm(event.target.value)}
+          />
+        </div>
+      </form>
+    </div>
+  );
 };
 
-class SearchBar extends React.Component {
-  state = { term: "" };
-  //   onInputChange = (event) => {
-  //     this.setState({ term: event.target.value });
-  //   };
+// class SearchBar extends React.Component {
+//   state = { term: "" };
+//   onInputChange = (event) => {
+//     this.setState({ term: event.target.value });
+//   };
+//   // ***************************************************
+//   onInputChange = (event) => {
+//     this.setState({ term: event.target.value });
+//   };
+//   onFormSubmit = (event) => {
+//     event.preventDefault();
+//     this.props.onFormSubmit(this.state.term);
+//   };
 
-  onInputChange = (event) => {
-    this.setState({ term: event.target.value });
-
-    //TODO: make sure we call call back from parent component
-  };
-  onFormSubmit = (event) => {
-    event.preventDefault();
-    this.props.onFormSubmit(this.state.term);
-  };
-
-  render() {
-    return (
-      <div className="search-bar ui segment">
-        <form onSubmit={this.onFormSubmit} action="" className="ui form">
-          <div className="field">
-            <label>videoSearch</label>
-            <input
-              type="text"
-              value={this.state.term}
-              onChange={this.onInputChange}
-            />
-          </div>
-        </form>
-      </div>
-    );
-  }
-}
+//   render() {
+//     return (
+//       <div className="search-bar ui segment">
+//         <form onSubmit={this.onFormSubmit} action="" className="ui form">
+//           <div className="field">
+//             <label>videoSearch</label>
+//             <input
+//               type="text"
+//               value={this.state.term}
+//               onChange={this.onInputChange}
+//             />
+//           </div>
+//         </form>
+//       </div>
+//     );
+//   }
+// }
 
 export default SearchBar;
