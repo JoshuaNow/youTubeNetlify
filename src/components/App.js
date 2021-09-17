17f096b9a7dd61c4cb54182e94479a97d2b742e1
Joshua Lopez <jrenelopez92@gmail.com>
2021-09-16 21:22:14 -0500

    finished refactoring added custom hook

--- src/components/App.js
+++ src/components/App.js
@@ -1,43 +1,36 @@
 import React, { useState, useEffect } from "react";
 import SearchBar from "./SearchBar";
-import youtube from "./youtube";
+// import youtube from "./youtube";
 import VideoList from "./VideoList";
 import VideoDetail from "./VideoDetail";
+import useVideos from "../hooks/useVideos";
 
 //TODO for after break, learn grid system with semantic ui
 //TODO fix default video when we re search another video
+// to make jsx resuable we use components, to make functions and calls, reusable was can do custom hooks
 
 function App() {
-  const [videos, setVideos] = useState([]);
   const [selectedVideo, setSelectedVideo] = useState(null);
+  const [videos, search] = useVideos("star wars");
 
   useEffect(() => {
-    onTermSubmit("star wars");
-  }, []);
-  const onTermSubmit = async (term) => {
-    const response = await youtube.get("/search", {
-      params: {
-        q: term,
-      },
-    });
+    setSelectedVideo(videos[0]);
+  }, [videos]);
 
-    setVideos(response.data.items);
-    setSelectedVideo(response.data.items[0]);
-  };
-  const onVideoSelect = (video) => {
-    setSelectedVideo(video);
-  };
+  // const onVideoSelect = (video) => {
+  //   setSelectedVideo(video);
+  // };
 
   return (
     <div className="ui container">
-      <SearchBar onFormSubmit={onTermSubmit} />{" "}
+      <SearchBar onFormSubmit={search} />{" "}
       <div className="ui grid">
         <div className="ui row">
           <div className="eleven wide column">
             <VideoDetail video={selectedVideo} />
           </div>
           <div className="five wide column">
-            <VideoList onVideoSelect={onVideoSelect} videos={videos} />
+            <VideoList onVideoSelect={setSelectedVideo} videos={videos} />
           </div>
         </div>
       </div>
